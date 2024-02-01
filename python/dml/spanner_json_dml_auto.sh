d1c4f8bdf872da7256cc2c7444553a351cb1a9f6
Hugo Rozestraten <hugosr@google.com>
2024-02-01 13:53:03 -0300

    Update spanner_json_dml_auto.sh

--- python/dml/spanner_json_dml_auto.sh
+++ python/dml/spanner_json_dml_auto.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 # usage
-# ./spanner_json_dml_auto.sh instance database table KeyType StartNumber ParallelProcess ExtraAttribute DataType RecordsPerRun
+# ./spanner_json_dml_auto.sh instance database table KeyType StartNumber ParallelProcess RecordsPerRun ExtraAttribute DataType 
 # example
-# ./spanner_json_dml_auto.sh test test mytable1 STRING 600000 80 attribu2 STRING 2000
+# ./spanner_json_dml_auto.sh test test mytable1 STRING 600000 80 2000 attribu2 STRING 
 
 
 instance=$1
@@ -11,11 +11,11 @@ table=$3
 type=$4
 startnum=$5
 parallel=$6
-attribute=$7
-attribute_type=$8
-records=$9
+records=$7
+attribute=$8
+attribute_type=$9
 
-
+#set initial number
 initial=$(($startnum))
 
 #print variables working
@@ -29,12 +29,24 @@ echo attribute: $attribute
 
 path=p_$parallel
 mkdir $path
+rm -rf $path/*
+PIDs=
 
+#launch parallel processes
 for i in $(seq 1 $parallel)
 do
     echo launch $initial to $(($initial+999))
     nohup python3 spanner_json_line_insert.py $instance $database $table $type $initial $(($initial+$records-1)) $attribute $attribute_type > $path/output_$initial.out 2>&1 &
+    PID=$!
+    PIDs="$PIDs $PID"
     initial=$(($initial+$records))
 done
 
+wait $PIDs
 
+
+c=0.0
+a=`cat $path/* | grep "records per second" | awk '{print $8}'`
+for b in $a; do c=`awk "BEGIN{ print $b + $c}"`; done
+datenow=`date '+%Y-%m-%d %H:%M:%S'`
+echo $path $datenow $initial_records $c records per second >>total.out
